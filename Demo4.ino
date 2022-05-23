79120ee6967a52e97a209c20db3f3476463c6b30
rake6682 <andy.t.ly3@gmail.com>
2022-05-22 21:32:17 -0700

    e

--- Demo4.ino
+++ Demo4.ino
@@ -2,6 +2,27 @@
 #include "DeviceDriverSet_xxx0.h"
 #include "ApplicationFunctionSet_xxx0.cpp"
 
+// Compiler was yelling at me for some reason so I decided to put the function here. 
+// It should be in DeviceDriverSet_xxx0.cpp though as a part of the DeviceDriverSet_Motor class though.
+// Please clean my code if you can. 
+int straight(ConquerorCarMotionControl direction, int distance /*In centimeters */) {
+  
+  int cmPerSecond = 79; // Calibrate based on surface
+
+  float msecondsToTravel = distance/cmPerSecond * 1000;
+
+  int millisecondsToTravel = round(msecondsToTravel);
+
+  ApplicationFunctionSet_ConquerorCarMotionControl(direction, 250 );
+
+  delay(millisecondsToTravel);
+
+  ApplicationFunctionSet_ConquerorCarMotionControl(stop_it, 250 );
+
+  return 0;
+  
+}
+
 DeviceDriverSet_Motor AppMotor;
 Application_xxx Application_ConquerorCarxxx0;
 MPU6050_getdata AppMPU6050getdata;
@@ -17,10 +38,11 @@ void setup() {
   delay(2000);
   AppMPU6050getdata.MPU6050_calibration();
   delay(2000);
-//  delay(10000);
-//  ApplicationFunctionSet_ConquerorCarMotionControl(status, 250 );  80 cm per second is the speed of the robot
-//  delay(1000);
-//  ApplicationFunctionSet_ConquerorCarMotionControl(stop_it, 250 );
+  ApplicationFunctionSet_ConquerorCarMotionControl(status, 250 );  
+  delay(1000);
+  ApplicationFunctionSet_ConquerorCarMotionControl(stop_it, 250 );
+
+//  straight(status, 122);
 
 }
 
@@ -40,31 +62,31 @@ void loop() {
 //    time1 = time2;
 //  }
 
-  AppMotor.turn(90); //Turns the robot 90 degrees clockwise, relative to its starting position.
-  delay(2000);
-  AppMotor.turn(-90); //Turns the robot 90 degrees counterclockwise, relative to its starting position.
-  delay(2000);
-  AppMotor.turn(0); //Turns robot to its starting position.
-  delay(2000);
-  AppMotor.turn(-120);
-  delay(2000);
-  AppMotor.turn(120);
-  delay(2000);
-  AppMotor.turn(150);
-  delay(2000);
-  AppMotor.turn(-150);
-  delay(2000);
-  AppMotor.turn(-180);
-  delay(2000);
-  AppMotor.turn(180);
-  delay(2000);
-  AppMotor.turn(-270);
-  delay(2000);
-  AppMotor.turn(270);
-  delay(2000);
-  AppMotor.turn(-360);
-  delay(2000);
-  AppMotor.turn(360);
-  delay(2000);
+//  AppMotor.turn(90); //Turns the robot 90 degrees clockwise, relative to its starting position.
+//  delay(2000);
+//  AppMotor.turn(-90); //Turns the robot 90 degrees counterclockwise, relative to its starting position.
+//  delay(2000);
+//  AppMotor.turn(0); //Turns robot to its starting position.
+//  delay(2000);
+//  AppMotor.turn(-120);
+//  delay(2000);
+//  AppMotor.turn(120);
+//  delay(2000);
+//  AppMotor.turn(150);
+//  delay(2000);
+//  AppMotor.turn(-150);
+//  delay(2000);
+//  AppMotor.turn(-180);
+//  delay(2000);
+//  AppMotor.turn(180);
+//  delay(2000);
+//  AppMotor.turn(-270);
+//  delay(2000);
+//  AppMotor.turn(270);
+//  delay(2000);
+//  AppMotor.turn(-360);
+//  delay(2000);
+//  AppMotor.turn(360);
+//  delay(2000);
   
 }
